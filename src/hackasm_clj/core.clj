f1de895d5c80e4a3c3c6b06e417d573a76d1bba6
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-08-15 12:22:50 +0100

    Add fn to load the source file

--- src/hackasm_clj/core.clj
+++ src/hackasm_clj/core.clj
@@ -1,7 +1,10 @@
 (ns hackasm-clj.core
   (:gen-class))
 
+(defn- load-src [src-file]
+  (line-seq (clojure.java.io/reader src-file)))
+
 (defn -main
-  "Assembles the HACK assembly program 'src'."
-  [src]
+  "Assembles the HACK assembly program 'src-file'."
+  [src-file]
   )
