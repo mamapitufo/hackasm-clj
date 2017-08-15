1fc29c9fef0ad6b8db25f9537df5f491fdc497e4
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-08-15 12:32:47 +0100

    Scrub instructions from the source file
    Implement a first pass at the source that removes comments and trims
    whitespace, leaving only the instructions.

--- src/hackasm_clj/core.clj
+++ src/hackasm_clj/core.clj
@@ -1,8 +1,19 @@
 (ns hackasm-clj.core
+  (:require [clojure.string :as s])
   (:gen-class))
 
 (defn- load-src [src-file]
   (line-seq (clojure.java.io/reader src-file)))
+
+(defn- scrub [line]
+  (-> line
+      (s/replace #"//.*$" "")
+      s/trim))
+
+(defn- first-pass [src]
+  (->> src
+       (map scrub)
+       (filter not-empty)))
 
 (defn -main
   "Assembles the HACK assembly program 'src-file'."
