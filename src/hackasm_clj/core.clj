d65292359b5997eb806ac34ddcba99a0f98641e4
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-08-16 16:47:34 +0100

    Translate C-Instructions
    This means the assembler now handles any assembly programs without symbols.

--- src/hackasm_clj/core.clj
+++ src/hackasm_clj/core.clj
@@ -1,6 +1,7 @@
 (ns hackasm-clj.core
   (:require [clojure.string :as s]
-            [clojure.pprint :as pprint])
+            [clojure.pprint :as pprint]
+            [hackasm-clj.tables :as tables])
   (:gen-class))
 
 (defn- load-src [src-file]
@@ -27,7 +28,12 @@
   (to-binary (subs instruction 1)))
 
 (defn- parse-c [instruction]
-  instruction)
+  (let [parts (re-matches #"^(?:([^=]+)=)?([^;]+)(?:;(.*))?$" instruction)
+        comp (tables/comp-table (keyword (parts 2)))
+        dest (tables/dest-table (keyword (parts 1)) "000")
+        jump (tables/jump-table (keyword (parts 3)) "000")]
+
+    (format "111%s%s%s" comp dest jump)))
 
 (defn- parse-instruction [instruction]
   (if (s/starts-with? instruction "@")
