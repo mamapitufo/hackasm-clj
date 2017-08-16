c5cfca1ed596c42b25bce6ef5de2a46298ce984c
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-08-16 15:16:52 +0100

    Parse A-Instructions
    Add A-Instruction translation, and go over all instructions and dispatch
    Translators for each instruction kind.

--- src/hackasm_clj/core.clj
+++ src/hackasm_clj/core.clj
@@ -1,5 +1,6 @@
 (ns hackasm-clj.core
-  (:require [clojure.string :as s])
+  (:require [clojure.string :as s]
+            [clojure.pprint :as pprint])
   (:gen-class))
 
 (defn- load-src [src-file]
@@ -19,10 +20,24 @@
        (map scrub)
        (filter not-empty)))
 
+(defn- to-binary [num-string]
+  (pprint/cl-format nil "~16,'0b" (Integer/parseInt num-string)))
+
+(defn- parse-a [instruction]
+  (to-binary (subs instruction 1)))
+
+(defn- parse-c [instruction]
+  instruction)
+
+(defn- parse-instruction [instruction]
+  (if (s/starts-with? instruction "@")
+    (parse-a instruction)
+    (parse-c instruction)))
+
 (defn -main
   "Assembles the HACK assembly program 'src-file'."
   [src-file]
   (let [instructions (first-pass (load-src src-file))
-        machine-instructions instructions]
+        machine-instructions (map parse-instruction instructions)]
 
     (write-results src-file machine-instructions)))
